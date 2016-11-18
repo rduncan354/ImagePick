01495d1be08b2eb553a28223c9dca74c25781767
rduncan354 <rduncan354@gmail.com>
2016-11-18 06:09:51 -0500

    Bantu's update of my project

--- app/src/main/java/de/vogella/android/imagepick/ImagePickActivity.java
+++ app/src/main/java/de/vogella/android/imagepick/ImagePickActivity.java
@@ -4,13 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,40 +17,28 @@ public class ImagePickActivity extends Activity {
     private static final int REQUEST_CODE = 1;
     private Bitmap bitmap;
     private ImageView imageView;
-    private Button button;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.result);
-        button = (Button) findViewById(R.id.button1);
-
+        final Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                pickImage(imageView);
+                pickImage(v);
             }
         });
-
+        imageView = (ImageView) findViewById(R.id.result);
     }
 
     public void pickImage(View View) {
-//        Intent intent = new Intent();
-//        //intent.setType("image/*");
-//        intent.setType("Pictures/Instagram/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
-//        startActivityForResult(intent, REQUEST_CODE);
-        String image_path = ;
-        File file = new File(image_path);
-        Uri uri = Uri.fromFile(file);
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent .setType("image/*");
-        intent .putExtra(Intent.EXTRA_STREAM, uri);
-        context.startActivity(intent );
-
-
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        // intent.setType("Pictures/Instagram/IMG_20150829_105921.jpg");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE);
 
     }
 
